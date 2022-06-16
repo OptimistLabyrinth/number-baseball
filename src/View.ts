99041599d62b40c9a6a66fef156eac9a21c004ea
ksyoo <ksyoo21remo@gmail.com>
2022-06-16 12:57:55 +0900

    arrange basic functionality as TODO
    Model, View, Controller
    each respoectively has its own role

---
+++ src/View.ts
@@ -0,0 +1,8 @@
+// * 4-2
+// TODO Controller 로부터 받은 사용자 입력에 대한 판단 결과가 "오류" 이면
+// TODO 오류 메세지를 생성해서 Controller 에게 돌려줌
+
+// * 8
+// TODO Controller 로부터 께산한 점수를 받음
+// TODO 점수에 따라 적절한 사용자 출력 메시지 생성
+// TODO 이렇게 생성한 사용자 출력을 Controller 에 전달
