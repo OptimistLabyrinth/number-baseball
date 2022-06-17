1aa57f920a89788fe16750bfc365e0788ca9451e
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 01:54:32 +0900

    feature: 4: modify validate to return false if either of the character in userInput is none of 1 ~ 9
    GREEN - REFACTOR starts

--- src/Model/NumberInputModel.ts
+++ src/Model/NumberInputModel.ts
@@ -1,5 +1,12 @@
 export default class NumberInputModel {
+  private possibleCharacters = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+
   validate(userInput: string) {
+    for (const each of userInput) {
+      if (!this.possibleCharacters.find((x) => x === each)) {
+        return false
+      }
+    }
     if (userInput.length !== 3)
       return false
     return true
