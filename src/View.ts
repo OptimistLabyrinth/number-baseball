a454fdfa7c6b33970911cf906cb23615e860ab35
ksyoo <ksyoo21remo@gmail.com>
2022-06-17 22:29:06 +0900

    arrange basic functionality as TODO 2
    added software requirements which I omitted
    after one game completes, demand user for next action

--- src/View.ts
+++ src/View.ts
@@ -1,8 +1,26 @@
-// * 4-2
+// * 1
+// TODO 사용자에게 숫자를 입력받기 위해서 메시지 출력 (stdout)
+
+// * 2
+// TODO 사용자로부터 입력을 받음 (stdin)
+// TODO 받은 사용자 입력을 Controller 로 전달
+
+// * 8
 // TODO Controller 로부터 받은 사용자 입력에 대한 판단 결과가 "오류" 이면
 // TODO 오류 메세지를 생성해서 Controller 에게 돌려줌
 
-// * 8
-// TODO Controller 로부터 께산한 점수를 받음
+// * 12
+// TODO Controller 로부터 계산한 점수를 받음
 // TODO 점수에 따라 적절한 사용자 출력 메시지 생성
-// TODO 이렇게 생성한 사용자 출력을 Controller 에 전달
+// TODO 사용자에게 메시지 출력 (stdout)
+
+// * 13
+// TODO 사용자에게 다음 동작을 요청 (stdout)
+
+// * 14
+// TODO 사용자가 선택한 다음 동작을 입력 받음 (stdin)
+// TODO 다음 동작에 대한 사용자 입력값을 Controller 에 전달
+
+// * 18
+// TODO 판단 결과로 "게임 새로 시작" 받음
+// TODO 종료 메시지 출력하며 프로그램 종료
