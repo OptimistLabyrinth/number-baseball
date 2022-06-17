6bc42f16897ea6d8e27a8b2b70967866855ade23
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 01:40:42 +0900

    feature: 4: add unit-test any character in parameter userInput is not one of 1 ~ 9 return false
    TDD principle, unit test first. RED

--- test/unit/Model/NumberInputModel.spec.ts
+++ test/unit/Model/NumberInputModel.spec.ts
@@ -41,5 +41,15 @@ describe('NumberInputModel 클래스', () => {
       expect(spy.calledOnce).to.be.equal(true)
       expect(valid).to.be.equal(false)
     })
+    it('파라미터 userInput 개별 요소가 하나라도 1 ~ 9 가 아니면 결과값 false 이다', () => {
+      if (spy === null) {
+        throw new Error('invalid sinon spy: null')
+      }
+      const numberInputModel = new NumberInputModel()
+      const userInput = 'abc'
+      const valid = numberInputModel.validate(userInput)
+      expect(spy.calledOnce).to.be.equal(true)
+      expect(valid).to.be.equal(false)
+    })
   })
 })
