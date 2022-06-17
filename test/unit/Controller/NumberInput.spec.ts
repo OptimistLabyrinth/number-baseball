5acbafa5d4fef93b936eef0c8f031e954f603997
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 00:30:31 +0900

    chore: add sinon spy to unit-test NumberInput.spec.ts

--- test/unit/Controller/NumberInput.spec.ts
+++ test/unit/Controller/NumberInput.spec.ts
@@ -5,10 +5,30 @@ import NumberInput from '../../../src/Controller/NumberInput'
 
 describe('NumberInput 클래스', () => {
   describe('setUserInput 메소드', () => {
+    let spy: sinon.SinonSpy | null = null
+
+    beforeEach(() => {
+      if (spy !== null) {
+        throw new Error('invalid sinon spy: failed to restore')
+      }
+      spy = sinon.spy(NumberInput.prototype, 'setUserInput')
+    })
+    afterEach(() => {
+      if (spy === null) {
+        throw new Error('invalid sinon spy: failed to setup')
+      }
+      spy.restore()
+      spy = null
+    })
+
     it('성공 시 NumberInput 객체의 userInput 필드의 값은 입력값과 동일하다', async () => {
+      if (spy === null) {
+        throw new Error('invalid sinon spy: null')
+      }
       const numberInput = new NumberInput()
       const userInput = '123'
       numberInput.setUserInput(userInput)
+      expect(spy.calledOnce).to.be.equal(true)
       expect(numberInput.userInput).to.be.equal(userInput)
     })
   })
