a53cafba6e977ac164f9b771da7a5733df496f32
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 08:45:28 +0900

    chore: short-circuit the logic of a method
    checking the length of the user input make the execution of the method simpler

--- src/Model/NumberInputModel.ts
+++ src/Model/NumberInputModel.ts
@@ -2,13 +2,13 @@ export default class NumberInputModel {
   private possibleCharacters = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
   validate(userInput: string): boolean {
+    if (userInput.length !== 3)
+      return false
     for (const each of userInput) {
       if (!this.possibleCharacters.find((x) => x === each)) {
         return false
       }
     }
-    if (userInput.length !== 3)
-      return false
     return true
   }
 }
