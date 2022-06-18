486db8225297623dbf0373f82de7452c1edaca0b
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 09:52:08 +0900

    chore: ignore prettier when it conflicts with eslint rule - dangling comma

--- src/View/index.ts
+++ src/View/index.ts
@@ -6,6 +6,7 @@ export default class View {
     const numberInputView = new NumberInputView()
     const numberInput = await numberInputView.request()
 
+    // prettier-ignore
     const numberInputController = new Controller.NumberInputController(
       numberInput,
     )
