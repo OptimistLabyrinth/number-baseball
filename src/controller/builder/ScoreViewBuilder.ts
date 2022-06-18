47518886bfbbea689d083d583c7060c903e2c982
ksyoo <ksyoo21remo@gmail.com>
2022-06-18 23:35:36 +0900

    feature: 11: implements ScoreViewBuilder class and add unit-test codes

--- src/controller/builder/ScoreViewBuilder.ts
+++ src/controller/builder/ScoreViewBuilder.ts
@@ -1,5 +1,10 @@
 import { ScoreModelDto } from '../../dto/ScoreModelDto'
 import ScoreView from '../../view/score-view/ScoreView'
+import ErrorScoreView from '../../view/score-view/ErrorScoreView'
+import BallsOnlyScoreView from '../../view/score-view/BallsOnlyScoreView'
+import NothingScoreView from '../../view/score-view/NothingScoreView'
+import StrikesAndBallsScoreView from '../../view/score-view/StrikesAndBallsScoreView'
+import StrikesOnlyScoreView from '../../view/score-view/StrikesOnlyScoreView'
 
 export default class ScoreViewBuilder {
   score: ScoreModelDto
@@ -9,6 +14,18 @@ export default class ScoreViewBuilder {
   }
 
   build(): ScoreView {
-    return new ScoreView()
+    if (this.score.error !== null) {
+      return new ErrorScoreView()
+    }
+    if (this.score.strikes === 3) {
+      return new StrikesOnlyScoreView()
+    }
+    if (0 < this.score.strikes && 0 < this.score.balls) {
+      return new StrikesAndBallsScoreView()
+    }
+    if (this.score.strikes === 0 && 0 < this.score.balls) {
+      return new BallsOnlyScoreView()
+    }
+    return new NothingScoreView()
   }
 }
--- test/unit/Controller/builder/ScoreViewBuilder.spec.ts
+++ test/unit/Controller/builder/ScoreViewBuilder.spec.ts
@@ -4,6 +4,14 @@ import * as sinon from 'sinon'
 // eslint-disable-next-line
 import ScoreViewBuilder from '../../../../src/controller/builder/ScoreViewBuilder'
 import { ScoreModelDto } from '../../../../src/dto/ScoreModelDto'
+import ErrorScoreView from '../../../../src/view/score-view/ErrorScoreView'
+// eslint-disable-next-line
+import BallsOnlyScoreView from '../../../../src/view/score-view/BallsOnlyScoreView'
+import NothingScoreView from '../../../../src/view/score-view/NothingScoreView'
+// eslint-disable-next-line
+import StrikesAndBallsScoreView from '../../../../src/view/score-view/StrikesAndBallsScoreView'
+// eslint-disable-next-line
+import StrikesOnlyScoreView from '../../../../src/view/score-view/StrikesOnlyScoreView'
 
 describe('ScoreViewBuilder 클래스', () => {
   describe('constructor', () => {
@@ -24,6 +32,122 @@ describe('ScoreViewBuilder 클래스', () => {
         expect(scoreField.strikes).to.be.equal(score.strikes)
         expect(scoreField.balls).to.be.equal(score.balls)
         expect(scoreField.nothings).to.be.equal(score.nothings)
+      },
+    )
+  })
+
+  describe('build 메소드', () => {
+    let spy: sinon.SinonSpy | null = null
+
+    beforeEach(() => {
+      if (spy !== null) {
+        throw new Error('invalid sinon spy: failed to restore')
+      }
+      spy = sinon.spy(ScoreViewBuilder.prototype, 'build')
+    })
+    afterEach(() => {
+      if (spy === null) {
+        throw new Error('invalid sinon spy: failed to setup')
+      }
+      spy.restore()
+      spy = null
+    })
+
+    it(
+      '필드 score 객체의 내용이 { error: \'오류 메시지를 담은 문자열\' } 이면 ' +
+        'ErrorScoreView 객체를 리턴한다',
+      () => {
+        if (spy === null) {
+          throw new Error('invalid sinon spy: null')
+        }
+        const score: ScoreModelDto = {
+          error: '오류 메시지',
+          strikes: 0,
+          balls: 0,
+          nothings: 0,
+        }
+        const scoreViewBuilder = new ScoreViewBuilder(score)
+        const result = scoreViewBuilder.build()
+        expect(spy.calledOnce).to.be.equal(true)
+        expect(result instanceof ErrorScoreView).to.be.equal(true)
+      },
+    )
+    it(
+      '필드 score 객체의 내용이 { error: null, strikes: 3 } 이면 ' +
+        'StrikesOnlyScoreView 객체를 리턴한다',
+      () => {
+        if (spy === null) {
+          throw new Error('invalid sinon spy: null')
+        }
+        const score: ScoreModelDto = {
+          error: null,
+          strikes: 3,
+          balls: 0,
+          nothings: 0,
+        }
+        const scoreViewBuilder = new ScoreViewBuilder(score)
+        const result = scoreViewBuilder.build()
+        expect(spy.calledOnce).to.be.equal(true)
+        expect(result instanceof StrikesOnlyScoreView).to.be.equal(true)
+      },
+    )
+    it(
+      '필드 score 객체의 내용이 ' +
+        '{ error: null, strikes: \'1 이상\', balls: \'1 이상\' } 이면 ' +
+        'StrikesAndBallsScoreView 객체를 리턴한다',
+      () => {
+        if (spy === null) {
+          throw new Error('invalid sinon spy: null')
+        }
+        const score: ScoreModelDto = {
+          error: null,
+          strikes: 1,
+          balls: 1,
+          nothings: 0,
+        }
+        const scoreViewBuilder = new ScoreViewBuilder(score)
+        const result = scoreViewBuilder.build()
+        expect(spy.calledOnce).to.be.equal(true)
+        expect(result instanceof StrikesAndBallsScoreView).to.be.equal(true)
+      },
+    )
+    it(
+      '필드 score 객체의 내용이 ' +
+        '{ error: null, strikes: 0, balls: \'1 이상\' } 이면 ' +
+        'BallsOnlyScoreView 객체를 리턴한다',
+      () => {
+        if (spy === null) {
+          throw new Error('invalid sinon spy: null')
+        }
+        const score: ScoreModelDto = {
+          error: null,
+          strikes: 0,
+          balls: 2,
+          nothings: 0,
+        }
+        const scoreViewBuilder = new ScoreViewBuilder(score)
+        const result = scoreViewBuilder.build()
+        expect(spy.calledOnce).to.be.equal(true)
+        expect(result instanceof BallsOnlyScoreView).to.be.equal(true)
+      },
+    )
+    it(
+      '필드 score 객체의 내용이 { error: null, strikes: 0, balls: 0 } 이면 ' +
+        'NothingScoreView 객체를 리턴한다',
+      () => {
+        if (spy === null) {
+          throw new Error('invalid sinon spy: null')
+        }
+        const score: ScoreModelDto = {
+          error: null,
+          strikes: 0,
+          balls: 0,
+          nothings: 3,
+        }
+        const scoreViewBuilder = new ScoreViewBuilder(score)
+        const result = scoreViewBuilder.build()
+        expect(spy.calledOnce).to.be.equal(true)
+        expect(result instanceof NothingScoreView).to.be.equal(true)
       },
     )
   })
